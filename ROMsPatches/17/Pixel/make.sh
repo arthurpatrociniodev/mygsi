b3fd3cb561e17a4d21368059394ac62f517908df
RofikKernelDev <rofikcoga@gmail.com>
2026-05-15 09:38:59 +0700

    Update make.sh

--- ROMsPatches/17/Pixel/make.sh
+++ ROMsPatches/17/Pixel/make.sh
@@ -3,23 +3,9 @@
 SCRIPT_DIR=$(dirname "$0")
 BASE_DIR="$1"
 
-if [ -d "$BASE_DIR/product" ] && [ ! -L "$BASE_DIR/product" ]; then
-    product="$BASE_DIR/product"
-elif [ -d "$BASE_DIR/system/product" ] && [ ! -L "$BASE_DIR/system/product" ]; then
-    product="$BASE_DIR/system/product"
-else
-    echo "error: No product dir"
-    exit 1
-fi
+product="$BASE_DIR/product"
 
-if [ -d "$BASE_DIR/system_ext" ] && [ ! -L "$BASE_DIR/system_ext" ]; then
     system_ext="$BASE_DIR/system_ext"
-elif [ -d "$BASE_DIR/system/system_ext" ] && [ ! -L "$BASE_DIR/system/system_ext" ]; then
-    system_ext="$BASE_DIR/system/system_ext"
-else
-    echo "error: No system_ext dir"
-    exit 1
-fi
 
 [ -f "$product/etc/selinux/product_seapp_contexts" ] && \
 sed -i "/dataservice_app/d" "$product/etc/selinux/product_seapp_contexts"
