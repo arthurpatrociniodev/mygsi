fc07914240dd132b62339c38b1305658bb84c841
RofikKernelDev <rofikcoga@gmail.com>
2026-05-15 09:45:09 +0700

    Update addvo.sh

--- Tools/vendoroverlay/addvo.sh
+++ Tools/vendoroverlay/addvo.sh
@@ -7,7 +7,7 @@ mkdir -p "$BASE_DIR/system/foxetgsi"
 mkdir -p "$BASE_DIR/system/foxetgsi/vo"
 
 cp -r "$BASE_DIR/vendor/overlay/." "$BASE_DIR/system/foxetgsi/vo/"
-cp "$BASE_DIR/vendor/etc/passwd" "$BASE_DIR/system/foxetgsi/"
-cp "$BASE_DIR/vendor/etc/group" "$BASE_DIR/system/foxetgsi/"
+cp "$BASE_DIR/vendor/etc/passwd" "$BASE_DIR/system/foxetgsi/passwd"
+cp "$BASE_DIR/vendor/etc/group" "$BASE_DIR/system/foxetgsi/group"
 
 echo "" >> "$BASE_DIR/system/bin/rw-system.sh" && cat "$SCRIPT_DIR/rw-system-add.sh" >> "$BASE_DIR/system/bin/rw-system.sh"
